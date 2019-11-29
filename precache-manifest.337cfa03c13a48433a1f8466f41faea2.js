6e2aecd04c91bfc56264b1e824a73652b171b60f
stinek4 <stinemvorner@gmail.com>
2019-11-29 09:49:42 +0100

    Updates

---
+++ precache-manifest.337cfa03c13a48433a1f8466f41faea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccec2ece26be1250a5813e5349236e4",
+    "url": "/humor/index.html"
+  },
+  {
+    "revision": "fa586b21c75afbb6b03b",
+    "url": "/humor/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f711327f979572ab3904",
+    "url": "/humor/static/js/2.b16fc17c.chunk.js"
+  },
+  {
+    "revision": "fa586b21c75afbb6b03b",
+    "url": "/humor/static/js/main.b24c0d8c.chunk.js"
+  },
+  {
+    "revision": "8205bb931e42c6572daa",
+    "url": "/humor/static/js/runtime-main.1ed82e56.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/humor/static/media/logo.25bf045c.svg"
+  }
+]);
